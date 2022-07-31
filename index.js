45761a8d7786cf94be20bb13aa9c98767aebf1a4
manymusa <adanalin113@gmail.com>
2022-07-31 19:19:29 -0400

    commit tree 771c616af38e250cae0fe7e6cb5f144f310e4f96

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 2499) {
+    return 'No can do.';
+  } else if (someValue >= 401) {
+    return 'I will gladly take your thirty bucks.';
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity) {
   // Write your code here!
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  switch(someTip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you." 
+    default :
+      return "Bye."
+  }
 }
